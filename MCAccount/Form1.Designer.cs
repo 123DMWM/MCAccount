4f67fb97236dbe47ec19073911bf1c64b3f25db2
123DMWM <shmo1joe2@gmail.com>
2015-06-14 20:27:28 -0400

    Initial Commit

---
+++ MCAccount/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows.Forms;
+namespace MCAccount {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(124, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "Account Name";
+			this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_Key);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(142, 10);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Lookup";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.textBox2.Location = new System.Drawing.Point(13, 39);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(204, 312);
+			this.textBox2.TabIndex = 2;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.Location = new System.Drawing.Point(223, 39);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(193, 312);
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(223, 8);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(192, 23);
+			this.button2.TabIndex = 4;
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(428, 362);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Minecraft Account History";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private Button button2;
+	}
+}
+
---
+++ MCAccount/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.IO;
+using ServiceStack.Text;
+
+namespace MCAccount {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			updatePage();
+			
+		}
+		private void button1_Key(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.Enter) {
+				updatePage();
+			}
+		}
+		private void updatePage() {
+			String uuid = getUUID(textBox1.Text);
+			if (uuid == null) {
+				MessageBox.Show("No player by that name");
+				return;
+			}
+			List<NameChange> names = JsonConvert.DeserializeObject<List<NameChange>>(getNames(uuid));
+			textBox2.ResetText();
+			DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+			for (int i = 0; i < names.Count; i++) {
+				DateTime changedAt = epoch.AddMilliseconds(double.Parse(names[i].changedToAt)).ToLocalTime();
+				textBox2.Text += (i + 1) + ". " + names[i].name + Environment.NewLine + (changedAt == epoch.ToLocalTime() ? "" : "	" + 
+					changedAt.ToShortDateString() + " at " + changedAt.ToLongTimeString() + Environment.NewLine);
+			}
+			pictureBox1.Image = getSkin(uuid);
+
+		}
+		private String getUUID(string name) {
+			JsonObject result = JsonObject.Parse((new WebClient()).DownloadString("https://api.mojang.com/users/profiles/minecraft/" + name));
+			try {
+				return result.Get("id");
+			} catch {
+				try {
+					result = JsonObject.Parse((new WebClient()).DownloadString("https://api.mojang.com/users/profiles/minecraft/" + name + "?at=0"));
+					return result.Get("id");
+				} catch (Exception ex) {
+					return null;
+				}
+			}
+		}
+		private String getNames(string uuid) {
+			return new StreamReader(new WebClient().OpenRead("https://api.mojang.com/user/profiles/" + uuid + "/names")).ReadToEnd();
+		}
+		private Image getSkin(string uuid) {
+			WebClient client = new WebClient();
+			Stream stream;
+			try {
+				stream = client.OpenRead("http://visage.gameminers.com/full/" + pictureBox1.Size.Height + "/" + uuid);
+				setSkinButton("1");
+				return Image.FromStream(stream);
+			} catch {
+				try {
+					stream = client.OpenRead("https://crafatar.com/renders/body/" + uuid + "?helm&scale=7");
+					setSkinButton("2");
+					return Image.FromStream(stream);
+				} catch {
+					MessageBox.Show("Failed to get skin. Main and backup websites might be down!");
+					return null;
+				}
+			}
+			
+		}
+		string skinURL;
+		private void setSkinButton(string urlNum) {
+			string urlHost = "Visage";
+			string urlLink = "http://visage.gameminers.com/";
+			switch (urlNum) {
+				case "2":
+					urlHost = "Crafatar";
+					urlLink = "https://crafatar.com/";
+					break;
+				default:
+					break;
+			}
+			button2.Text = "Skin rendered using " + urlHost;
+			skinURL = urlLink;
+		}
+
+		private void button2_Click(object sender, EventArgs e) {
+			System.Diagnostics.Process.Start(skinURL);
+
+		}
+	}
+	class NameChange { public String name { get; set; } public String changedToAt = "0"; }
+	class Skins { public String SKIN { get; set; } public String url { get; set; } }
+}
